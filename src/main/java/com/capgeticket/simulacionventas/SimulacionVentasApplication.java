bff71416b82d4dd142083451dc2faaee18335df8
Sergio Salgueiro Monforte <90696985+Sergiosalgueiro01@users.noreply.github.com>
2024-10-23 13:40:13 +0200

    Add files via upload

---
+++ src/main/java/com/capgeticket/simulacionventas/SimulacionVentasApplication.java
@@ -0,0 +1,13 @@
+package com.capgeticket.simulacionventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacionVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacionVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgeticket/simulacionventas/SimulacionVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgeticket.simulacionventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacionVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
